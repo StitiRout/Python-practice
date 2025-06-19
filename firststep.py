9566bba9fbd37d4e749b3d811addd7ab2fb3e693
StitiRout <stitirout2024@gmail.com>
2025-06-19 12:17:06 +0530

    first step towards python

---
+++ firststep.py
@@ -0,0 +1,69 @@
+print("hello world")
+print ("Stiti is my name ,my age is 20")
+print(23)
+print(34+56)
+name="stiti"
+AGE=20
+price=8.99
+print(name,AGE,price)
+print(type(name))
+print(type(AGE))
+print(type(price))
+old= True
+a= None
+print(old,a)
+print(type(old))
+print(type(a))
+# arithmeticOperators
+a1=4
+a2= 19
+sum=a1+a2
+diff=a2-a1
+mul=a1*a2
+div=a1/a2
+divv=a1//a2
+modulo=a1%a2
+print(sum)
+print(diff)
+print(mul)
+print(div)
+print(divv)
+print(modulo)
+print(a1**a2)
+#  relationaloperators
+b1=50
+b2=57
+print(b1==b2)
+print(b1!=b2)
+print(b1>=b2)
+print(b1<=b2)
+#  assignment operators
+num = 10
+num += 10 # num = num+10
+# num -=10
+# num *=10
+# num /=10
+# num %=10
+# num **=10
+print("final number",num)
+# logical operators
+# print(not True)
+# print(not False)
+#  c1 = 29
+#  c2 = 92
+#  print(not (c1 > c2))
+val1=True
+val2=True
+print("and operator:",val1 and val2)
+val3=False
+print("and operator:",val1 and val3)
+print("or opertor:",val1 or val3)
+print("or operator:",(b1==b2) or (b1>b2))
+# conversion
+value1=45
+value2=23.99
+print(value1+value2)
+value3=int("45")
+print(value1+value3)
+# value=float("89")
+ 
---
+++ userinput.py
@@ -0,0 +1,17 @@
+# name=input("Enter your name :")
+#  print("Welcome",name)
+# print(type(name))
+# age=int(input("Enter yout age"))
+# slno= int(input("Enter your sl no."))
+# print("hello,Namaste  ",name,"your age is :" ,age,"and your sl no. is: ",slno)
+# a=int(input("Enter an integer :  "))
+# b=int(input("Enter a second integer:  "))
+ # sum=a+b
+# print("sum of the 2 value is :  ",sum)
+# print("area of the square is: " ,a*a)
+c=float(input("Enter a number :  "))
+d=float(input("Enter a second number:  "))
+sum=c+d
+avg=sum/2
+print("average of these numbers are ",avg)
+print((c>=d) or (c==d))
