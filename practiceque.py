260679b35a5719a89297b391bb1faf2bceff4659
StitiRout <stitirout2024@gmail.com>
2025-06-23 11:33:41 +0530

    question on String and conditional

---
+++ practiceque.py
@@ -0,0 +1,15 @@
+# # question1
+# str1=input("Enter your name:")
+# print(str1)
+# print("length of your name is  ",len(str1))
+# # question2
+# print(str1.find("prangya"))
+# print(str1.count("pr"))
+# question3
+a=int(input("Enter your age"))
+if(a>18):
+    print("you are able to vote")
+elif(a<=18):
+    print("you are not able to vote")
+else:
+    print("invalid input")        
