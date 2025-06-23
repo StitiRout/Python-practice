131e24265f20af50acdbf7110650cf664bcaa23f
StitiRout <stitirout2024@gmail.com>
2025-06-23 11:31:55 +0530

    conditional

---
+++ conditional.py
@@ -0,0 +1,19 @@
+# if-elif-else
+# a=int(input("Enter an integer"))
+# b=int(input("Enter an second integer"))
+# if(a<=b):
+#     print("a is smaller")
+# elif(a>=b):
+#     print("a is greater") 
+# else:
+#     print("both are equal")       
+# nesting
+age=34
+if(age>=21):
+    if(age>=18):
+        print("You can get driving license")
+    else:
+        print("You cannot get a driving license")
+    print("You can marry") 
+else:
+    print("you cannot marry")          
