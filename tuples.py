5ed7e7cb31637a6c3c3f5c5a6c9d869e5f33c211
StitiRout <stitirout2024@gmail.com>
2025-07-11 12:07:51 +0530

    practise que done

--- tuples.py
+++ tuples.py
@@ -1,14 +1,22 @@
 # tuple
-number = (2,3,7,5,0,1,2)
-print(type(number))
-num=(32,)
-print(type(num))
-numb=(4)
-print(type(numb))
+# number = (2,3,7,5,0,1,2)
+# print(type(number))
+# num=(32,)
+# print(type(num))
+# numb=(4)
+# print(type(numb))
 # tuple slicing
-print(number[1:5])
+# print(number[1:5])
 # tuple method
-number.index(0)
-print(number)
-number.count(2)
-print(number)
+# print(number.index(0))
+# print(number.count(2))
+
+# practice que on tuple
+# WAP TO COUNT THE NUMBER OF THE STUDENTS WITH THE "A" GRADE IN THE FOLLOWING TUPLE
+# grade = ("A","B","C","A","A","D","B")
+# print(grade.count("A"))
+
+# Store the above values in a list and sort them from a to d
+grade = ["A","B","C","A","A","D","B"]
+grade.sort()
+print(grade)
