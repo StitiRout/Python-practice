f752728326acf3f281df2cbd1ea3ffbe98ebd9dc
StitiRout <stitirout2024@gmail.com>
2025-07-11 11:26:57 +0530

    learning tuples

---
+++ tuples.py
@@ -0,0 +1,14 @@
+# tuple
+number = (2,3,7,5,0,1,2)
+print(type(number))
+num=(32,)
+print(type(num))
+numb=(4)
+print(type(numb))
+# tuple slicing
+print(number[1:5])
+# tuple method
+number.index(0)
+print(number)
+number.count(2)
+print(number)
