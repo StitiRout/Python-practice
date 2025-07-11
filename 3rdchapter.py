a3a39aa276d09323c9079d96eb846b40d53b3809
StitiRout <stitirout2024@gmail.com>
2025-07-11 11:11:02 +0530

    learning about list

---
+++ 3rdchapter.py
@@ -0,0 +1,21 @@
+# lists
+# marks=[32.3,45.7,56.7,89.8]
+# print(marks)
+# print(type(marks))
+# print(marks[0],marks[1])
+# student=["rakesh",89.9,"kharagpur"]
+# print(student)
+# print(marks[1:3])
+# print(marks[-3:-1])
+# list method
+number = [2,6,4,0,1]
+number.sort()
+print(number)    #in ascending order
+number.sort(reverse=True)                #in descending order
+print(number)       
+number.reverse() 
+print(number)                  #in reverse order
+number.append(10)
+print(number)                 #add a number at end
+number.insert(0,29)    #add a number at the given index
+print(number)
