36f8312ab1a167ad19a2dc067bef5393caaeab3b
StitiRout <stitirout2024@gmail.com>
2025-07-14 12:49:11 +0530

    dictionary

---
+++ 4thchapter.py
@@ -0,0 +1,27 @@
+# dictionary 
+info = {
+    "key " : "values",
+    "name" : "stiti",
+    "learning" : "coding"
+}
+print(info)
+# things we can use in dictionary are ---
+student = { 
+    "name " : "Shree",
+    "sl no" : 42,
+    "cgpa"  : 8.6,
+    "is_pass" : True,
+    "language" : ["java","python","javascript","sql"],     
+    "programming" : ("java","python")
+}
+print(student)
+print(type(student))
+print(info["name"])
+print(student["name "])
+print(student["sl no"])
+print(student["cgpa"])
+print(student["is_pass"])
+print(student["language"])
+print(student["programming"])
+print(info["key "])
+print(info["learning"])
