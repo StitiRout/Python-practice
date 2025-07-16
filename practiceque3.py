ece2b713c0c51ff628303b3a0639b3e4a239f42c
StitiRout <stitirout2024@gmail.com>
2025-07-16 21:10:53 +0530

    practice question

--- practiceque3.py
+++ practiceque3.py
@@ -4,8 +4,21 @@ dict_1={
     "cat" : "a small animal"
 }
 print(dict_1)
+
+
 # you are given a list of subjects for students .Assume one classroom is required for 1 subject .
 # how many classrooms are needed by all students
 classroom={"python","java","c++","python","javascript","java","python","java","c++","c"}
 print(classroom)
 print(len(classroom))
+
+# WAP to enter marks of 3 subjects from the user and store them in a dictionary .Start with an empty dictionary and add
+# one by one .Use subject as key and marks as values
+markings={}
+mark1=int(input("Enter maths : "))
+markings.update({"maths":mark1})
+mark2=int(input("Enter phy : "))
+markings.update({"phy":mark2})
+mark3=int(input("Enter chem : "))
+markings.update({"chem":mark3})
+print(markings)
