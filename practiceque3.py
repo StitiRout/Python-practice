d7e969bc652a39a9ac2f09b9be1c02525e5bf729
StitiRout <stitirout2024@gmail.com>
2025-07-16 20:54:43 +0530

    practic que

---
+++ practiceque3.py
@@ -0,0 +1,11 @@
+# Store the following word meaning in a python dictionary
+dict_1={
+    "tables" : ("a piece of furniture","list of facts and figures"),
+    "cat" : "a small animal"
+}
+print(dict_1)
+# you are given a list of subjects for students .Assume one classroom is required for 1 subject .
+# how many classrooms are needed by all students
+classroom={"python","java","c++","python","javascript","java","python","java","c++","c"}
+print(classroom)
+print(len(classroom))
