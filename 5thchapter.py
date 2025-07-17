c91b681efc996e6da8ef861ad6a32cc6bef84ba5
StitiRout <stitirout2024@gmail.com>
2025-07-17 19:54:26 +0530

    while loop practice

---
+++ 5thchapter.py
@@ -0,0 +1,37 @@
+# loops
+# while loops
+# count=1
+# while count <=5:
+#     print("hello")
+#     count +=1
+
+# print(count)
+ 
+# i = 0
+# while i <=9:
+#     print("Stiti")
+#     i+=1
+
+# print from 1 to 100
+# j=1
+# while j<=100:
+#     print(j)
+#     j+=1
+# #print from 100 to 1
+# k=100
+# while k >=1:
+#     print(k)
+#     k-=1 
+
+# print the multiplication table of a number n
+# mul=int(input("Enter the number for your multiplication table"))
+# s=1
+# while s <=10:
+#     print(mul,"x",s,"=",mul*s)
+#     s+=1
+
+# print the elements of the following list sing a loop
+num=1
+while num<=10:
+    print(num*num)
+    num+=1
