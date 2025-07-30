1bd8c84fe4fa4225b15c9f3a841224b6c5ca8a34
StitiRout <stitirout2024@gmail.com>
2025-07-30 11:33:04 +0530

    read and write,with syntax

--- 7thchapter.py
+++ 7thchapter.py
@@ -1,5 +1,23 @@
-f = open("demo.txt","r")
-data = f.read()
-print(data)
-print(type(data))
-f.close()
+# File i/o
+# reading
+# f = open("demo.txt","r")
+# data = f.read(5)
+# print(data)
+# print(type(data))
+# line1 = f.readline()
+# print(line1)
+# line2=f.readline()
+# print(line2)
+
+# writing
+g=open("demo.txt","w")
+g.write("this a new line")
+
+g1=open("demo.txt","a")
+g1.write("this is added at the end of the line ")
+
+
+
+
+# f.close()
+g.close()
